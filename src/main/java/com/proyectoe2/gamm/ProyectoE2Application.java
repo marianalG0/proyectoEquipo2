0c5c8977fccbab5bf7249e59c9b69b45fb30dc63
marianalG0 <garciahaumariana@gmail.com>
2022-09-23 12:24:17 -0500

    Subiendo la primera parte para editar
    Subiendo la primera parte para editar

---
+++ src/main/java/com/proyectoe2/gamm/ProyectoE2Application.java
@@ -0,0 +1,13 @@
+package com.proyectoe2.gamm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoE2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoE2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoe2/gamm/ProyectoE2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoe2.gamm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoE2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
