3915faf6412cc13e683656e7cad85b39e6556e22
melaniebalam <melanie.pucbalam@cbtis72.edu.mx>
2022-09-24 08:59:37 -0500

    Subiendo las secciones de la tabla
    Realize las tablas

---
+++ src/main/java/com/proyectoe2/gamm/service/IComidasService.java
@@ -0,0 +1,10 @@
+package com.proyectoe2.gamm.service;
+
+import java.util.List;
+
+import com.proyectoe2.gamm.model.Comida;
+
+public interface IComidasService {
+	List<Comida> buscarTodo();
+	Comida buscarPorId(Integer idComida);
+}
