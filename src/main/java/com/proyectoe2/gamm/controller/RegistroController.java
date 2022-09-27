9667abda70cbc6c6a48c61782e4cb076ff13774a
marianalG0 <garciahaumariana@gmail.com>
2022-09-27 16:38:45 -0500

    Cambios

--- src/main/java/com/proyectoe2/gamm/controller/RegistroController.java
+++ src/main/java/com/proyectoe2/gamm/controller/RegistroController.java
@@ -24,15 +24,15 @@ public class RegistroController {
 	}
 	// @PostMapping("/save")
 	@RequestMapping(value="/save", method=RequestMethod.GET)
-	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("precio") String precio,@RequestParam("descripcion") String descripcion,@RequestParam("descuento") String descuento, Model model) {
+	public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion, Model model) {
 		System.out.println("nombre: " + nombre);
-		System.out.println("precio: " + precio);
+	
 		System.out.println("descripcion: " + descripcion);
-		System.out.println("descuento: " + descuento);
+		
 		model.addAttribute("nombre", nombre);
-		model.addAttribute("precio", precio);
+		
 		model.addAttribute("descripcion", descripcion);
-		model.addAttribute("descuento", descuento);
+		
 		
 		return "Registro/ListaRegistros";
 	
